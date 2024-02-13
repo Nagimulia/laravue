f9aec186e89c96e082db11f2fd3d1571edbc2d9f
Keneuova Nagima <nagimakeneuova@gmail.com>
2024-02-13 20:06:16 +0600

    build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -17,4 +18,14 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            '~': path.join(__dirname, '/node_modules/'),
+        }
+    },
+    build: {
+        chunkSizeWarningLimit: 1600,
+    },
 });
+
+
